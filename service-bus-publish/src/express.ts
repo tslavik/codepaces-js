8db4d632d2e2daad0c3b83695200ca0ac753ed02
Tomas Slavik <tomas.slavik@microsoft.com>
2020-12-16 18:27:44 +0100

    try custom query

--- service-bus-publish/src/express.ts
+++ service-bus-publish/src/express.ts
@@ -1,15 +1,23 @@
 var express = require('express');
 var app = express();
 app.post("/foo", (req, res) => {
+
     //just for OSSAR
     let injection = "Hello, security vulnerabilities!";
     eval(`console.log(\"${injection}\");`);
-    
+
     var obj = req.body;
     var ret = [];
-    // Potential DoS if obj.length is large.
+    // Potential DoS if obj.length is large. CodeQL
     for (var i = 0; i < obj.length; i++) {
         ret.push(obj[i]);
+    }
+
+    //Custom CodeQL Query
+    var x;
+    x == 123;
+    if(x>100){
+        //do anything
     }
 });
 
